9b27da997fe3fe80ef6763b589921b299c8c4e9b
Carine Todmia <ctodmia@gmail.com>
2024-01-29 08:01:27 -0500

    initial gatsby site

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,8 +8,11 @@ const config: GatsbyConfig = {
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
+
+   /**"gatsby-plugin-google-gtag"*/
+   
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
